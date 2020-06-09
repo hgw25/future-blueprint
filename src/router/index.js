b55c3c33ea54a34ce547371ae55092b5caf3494b
heguangwei <52685814@qq.com>
2020-06-09 10:18:01 +0800

    提交

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: resolve => require(["@/pages/home"], resolve)
+  }
+]
+export default new Router({
+  mode: 'history',
+  routes,
+})
